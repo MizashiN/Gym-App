642c72dc680a8933d0b1a720d1e22d37d48e6c7b
Matheus Ortiz <matheusbasket22@gmail.com>
2024-11-01 13:40:42 -0300

    Comboboxes Created

--- Frame_Supplements.pas
+++ Frame_Supplements.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
   Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
-  AdvCombo, AdvDBComboBox, AdvDBLookupComboBox;
+  AdvCombo, AdvDBComboBox, AdvDBLookupComboBox, Vcl.DBCtrls;
 
 type
   TSupplements = class(TFrame)
@@ -15,11 +15,21 @@ type
     Label2: TLabel;
     Shape2: TShape;
     Panel3: TPanel;
-    Panel5: TPanel;
-    Panel7: TPanel;
-    Panel8: TPanel;
-    Panel9: TPanel;
-    Panel4: TPanel;
+    SubcategoriesPanel: TPanel;
+    BrandPanel: TPanel;
+    MaxPricePanel: TPanel;
+    MinPricePanel: TPanel;
+    CategoryPanel: TPanel;
+    BrandsCombobox: TDBComboBox;
+    Label1: TLabel;
+    CategoryCombobox: TDBComboBox;
+    Label3: TLabel;
+    SubcategoriesCombobox: TDBComboBox;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    MinPriceEdit: TEdit;
+    MaxPriceEdit: TEdit;
   private
     { Private declarations }
   public
