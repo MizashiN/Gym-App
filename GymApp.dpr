20c905ea883c6f791cd7fb79a91841ba570a5a6d
Matheus Ortiz <matheusbasket22@gmail.com>
2024-11-29 00:07:14 -0300

    a

--- GymApp.dpr
+++ GymApp.dpr
@@ -4,7 +4,8 @@ uses
   Vcl.Forms,
   Global_Functions in 'Global_Functions.pas',
   DM_Connection in 'DM_Connection.pas' {DM_Con: TDataModule},
-  Form_Supplements in 'Form_Supplements.pas' {Supplements};
+  Form_Supplements in 'Form_Supplements.pas' {Supplements},
+  Form_ConfigCompanies in 'Form_ConfigCompanies.pas' {Form1};
 
 {$R *.res}
 
@@ -12,5 +13,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TDM_Con, DM_Con);
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
